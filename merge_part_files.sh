31406cca40e21c1a9ec6ecbbc5d656e780a39a71
Yadi <shahryary@gmail.com>
2020-09-23 13:27:47 +0200

    csv split

---
+++ merge_part_files.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Merging all csvfiles inside of folders into one csv file. 
+# copy to the root of folders 
+#--------------- 
+if [ ! -d merged_csv ]; then
+	mkdir merged_csv
+fi
+#---------------
+
+for folder in */
+do
+
+		new_file=$(basename "$folder")
+		echo "================================================="
+		echo "merging files in folder: " $new_file
+
+		head -1 $folder/part-1-100000-*.csv > $folder/$new_file.csv
+		for file in $(ls -rt $folder/part-*)
+		do
+			tail -n +2 -q $file >> $folder/$new_file.csv		
+		done
+		mv $folder/$new_file.csv merged_csv/
+
+		echo "number of lines: " $(wc -l merged_csv/$new_file.csv)
+		echo "================================================="
+
+done
+
