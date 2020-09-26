b1f3134d3fc51a914901e0c876bfc8e6e513d8c5
Yadi <shahryary@gmail.com>
2020-09-26 09:05:24 +0200

    updated file name

--- merge_part_files.sh
+++ merge_part_files.sh
@@ -15,9 +15,10 @@ do
 		echo "================================================="
 		echo "merging files in folder: " $new_file
 
-		head -1 $folder/part-1-100000-*.csv > $folder/$new_file.csv
-		for file in $(ls -rt $folder/part-*)
+		head -1 $folder/*-100000-*.csv > $folder/$new_file.csv
+		for file in $(ls -rt $folder/*)
 		do
+			echo "reading " $file
 			tail -n +2 -q $file >> $folder/$new_file.csv		
 		done
 		mv $folder/$new_file.csv merged_csv/
